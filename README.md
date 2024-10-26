c800c4de026f9bf6f457c3025c74a70307096dbe
Victor <victorcmitchell@gmail.com>
2024-10-26 13:09:06 +0900

    add Eigen dependency

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # digital-filters
+
+## Dependencies
+
+### Eigen
+
+Install Eigen by apt:
+```
+sudo apt update
+sudo apt install libeigen3-dev
+```
+Or install by following the [Eigen wiki](https://eigen.tuxfamily.org/dox/GettingStarted.html)
--- kalman_filter/main.cpp
+++ kalman_filter/main.cpp
@@ -3,14 +3,14 @@
 #include <ctime>
 #include <Eigen/Dense>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     std::cout << "Kalman Filter Test" << std::endl;
     std::srand(std::time(0));
     const auto start = std::chrono::system_clock::now();
 
     double elapsedS = 0;
-    while(elapsedS < 5.0)
+    while (elapsedS < 5.0)
     {
         const auto timestamp = std::chrono::system_clock::now();
         elapsedS = static_cast<double>(std::chrono::duration_cast<std::chrono::milliseconds>(timestamp - start).count()) / 1000.0;
